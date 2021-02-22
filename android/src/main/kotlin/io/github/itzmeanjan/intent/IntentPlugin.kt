1c9262934f17707c453ef28c6e7718b2341956f7
ssk <developer.kss@gmail.com>
2021-02-22 22:29:36 +0800

    extract method createIntentWithMethodCall

--- android/src/main/kotlin/io/github/itzmeanjan/intent/IntentPlugin.kt
+++ android/src/main/kotlin/io/github/itzmeanjan/intent/IntentPlugin.kt
@@ -80,95 +80,8 @@ class IntentPlugin(private val registrar: Registrar, private val activity: Activ
         when (call.method) {
             // when we're not interested in result of activity started, we call this method via platform channel
             "startActivity" -> {
-                val intent = Intent()
-                intent.action = call.argument<String>("action")
-                if (call.argument<String>("package") != null)
-                    intent.`package` = call.argument<String>("package")
-                
-                val _data = call.argument<String>("data")
-                val _type = call.argument<String>("type")
-                if (_data != null && _type == null)
-                    intent.data = Uri.parse(_data);
-                if (_type != null && _data == null)
-                    intent.type = _type;
-                if (_type != null && _data != null) {
-                    intent.setDataAndType(Uri.parse(_data), _type);
-                }
+                val intent = createIntentWithMethodCall(call)
 
-                // typeInfo parsed into associative array, which can be used for type casting extra data
-                val typeInfo = call.argument<Map<String, String>>("typeInfo")
-
-                call.argument<Map<String, Any>>("extra")?.apply {
-                    this.entries.forEach {
-                        when (it.key) {
-                            Intent.EXTRA_DONT_KILL_APP,
-                            Intent.EXTRA_LOCAL_ONLY,
-                            Intent.EXTRA_ALLOW_MULTIPLE, Intent.EXTRA_PROCESS_TEXT_READONLY -> intent.putExtra(it.key, it.value as Boolean)
-                            Intent.EXTRA_EMAIL,
-                            Intent.EXTRA_BCC,
-                            Intent.EXTRA_CC, Intent.EXTRA_MIME_TYPES -> {
-                                val tmp = it.value as ArrayList<*>
-                                intent.putExtra(it.key, tmp.toArray(arrayOfNulls<String>(tmp.size)))
-                            }
-                            Intent.EXTRA_CONTENT_ANNOTATIONS -> intent.putExtra(it.key, (it.value as? ArrayList<*>)?.filterIsInstance<String>() as ArrayList<String>)
-                            Intent.EXTRA_ORIGINATING_URI -> intent.putExtra(it.key, it.value as Uri)
-                            Intent.EXTRA_PROCESS_TEXT, Intent.EXTRA_TEXT, Intent.EXTRA_TITLE -> {
-                                if (listOf(Intent.ACTION_WEB_SEARCH, Intent.ACTION_SEARCH).contains(intent.action!!))
-                                    intent.putExtra("query", it.value as CharSequence)
-                                else
-                                    intent.putExtra(it.key, it.value as CharSequence)
-                            }
-                            // here in this block, we'll try to leverage type information
-                            // field passed via platform channel
-                            else -> {
-                                // if type information for this extra key is
-                                // provided by developer, then use that type information
-                                if (typeInfo?.containsKey(it.key)!!) {
-
-                                    when (typeInfo[it.key]) {
-                                        // casting into singular types
-
-                                        "boolean" -> intent.putExtra(it.key, it.value as Boolean)
-                                        "byte" -> intent.putExtra(it.key, it.value as Byte)
-                                        "short" -> intent.putExtra(it.key, it.value as Short)
-                                        "int" -> intent.putExtra(it.key, it.value as Int)
-                                        "long" -> intent.putExtra(it.key, it.value as Long)
-                                        "float" -> intent.putExtra(it.key, it.value as Float)
-                                        "double" -> intent.putExtra(it.key, it.value as Double)
-                                        "char" -> intent.putExtra(it.key, it.value as Char)
-                                        "String" -> intent.putExtra(it.key, it.value as String)
-
-                                        // casting into plural types
-
-                                        "boolean[]" -> intent.putExtra(it.key, it.value as BooleanArray)
-                                        "byte[]" -> intent.putExtra(it.key, it.value as ByteArray)
-                                        "short[]" -> intent.putExtra(it.key, it.value as ShortArray)
-                                        "int[]" -> intent.putExtra(it.key, it.value as IntArray)
-                                        "long[]" -> intent.putExtra(it.key, it.value as LongArray)
-                                        "float[]" -> intent.putExtra(it.key, it.value as FloatArray)
-                                        "double[]" -> intent.putExtra(it.key, it.value as DoubleArray)
-                                        "char[]" -> intent.putExtra(it.key, it.value as CharArray)
-                                        "String[]" -> {
-                                            val tmp = it.value as ArrayList<*>
-                                            intent.putExtra(it.key, tmp.toArray(arrayOfNulls<String>(tmp.size)))
-                                        }
-                                        // if some unsupported type information supplied by user
-                                        else -> intent.putExtra(it.key, it.value as String)
-                                    }
-
-                                } else {
-                                    intent.putExtra(it.key, it.value as String)
-                                }
-                            }
-                        }
-                    }
-                }
-                call.argument<List<Int>>("flag")?.forEach {
-                    intent.addFlags(it)
-                }
-                call.argument<List<String>>("category")?.forEach {
-                    intent.addCategory(it)
-                }
                 try {
                     if (call.argument<Boolean>("chooser")!!) activity.startActivity(Intent.createChooser(intent, call.argument<String>("chooserTitle") ?: "Sharing"))
                     else activity.startActivity(intent)
@@ -188,95 +101,8 @@ class IntentPlugin(private val registrar: Registrar, private val activity: Activ
                 }
                 val activityImageVideoCaptureCode = 998
                 val activityIdentifierCode = 999
-                val intent = Intent()
-                intent.action = call.argument<String>("action")
-                if (call.argument<String>("package") != null)
-                    intent.`package` = call.argument<String>("package")
-                
-                val _data = call.argument<String>("data")
-                val _type = call.argument<String>("type")
-                if (_data != null && _type == null)
-                    intent.data = Uri.parse(_data);
-                if (_type != null && _data == null)
-                    intent.type = _type;
-                if (_type != null && _data != null) {
-                    intent.setDataAndType(Uri.parse(_data), _type);
-                }
-                
-                // typeInfo parsed into associative array, which can be used for type casting extra data
-                val typeInfo = call.argument<Map<String, String>>("typeInfo")
+                val intent = createIntentWithMethodCall(call)
 
-                call.argument<Map<String, Any>>("extra")?.apply {
-                    this.entries.forEach {
-                        when (it.key) {
-                            Intent.EXTRA_DONT_KILL_APP,
-                            Intent.EXTRA_LOCAL_ONLY,
-                            Intent.EXTRA_ALLOW_MULTIPLE, Intent.EXTRA_PROCESS_TEXT_READONLY -> intent.putExtra(it.key, it.value as Boolean)
-                            Intent.EXTRA_EMAIL,
-                            Intent.EXTRA_BCC,
-                            Intent.EXTRA_CC, Intent.EXTRA_MIME_TYPES -> {
-                                val tmp = it.value as ArrayList<*>
-                                intent.putExtra(it.key, tmp.toArray(arrayOfNulls<String>(tmp.size)))
-                            }
-                            Intent.EXTRA_CONTENT_ANNOTATIONS -> intent.putExtra(it.key, (it.value as? ArrayList<*>)?.filterIsInstance<String>() as ArrayList<String>)
-                            Intent.EXTRA_ORIGINATING_URI -> intent.putExtra(it.key, it.value as Uri)
-                            Intent.EXTRA_PROCESS_TEXT, Intent.EXTRA_TEXT, Intent.EXTRA_TITLE -> {
-                                if (listOf(Intent.ACTION_WEB_SEARCH, Intent.ACTION_SEARCH).contains(intent.action!!))
-                                    intent.putExtra("query", it.value as CharSequence)
-                                else
-                                    intent.putExtra(it.key, it.value as CharSequence)
-                            }
-                            // here in this block, we'll try to leverage type information
-                            // field passed via platform channel
-                            else -> {
-                                // if type information for this extra key is
-                                // provided by developer, then use that type information
-                                if (typeInfo?.containsKey(it.key)!!) {
-
-                                    when (typeInfo[it.key]) {
-                                        // casting into singular types
-
-                                        "boolean" -> intent.putExtra(it.key, it.value as Boolean)
-                                        "byte" -> intent.putExtra(it.key, it.value as Byte)
-                                        "short" -> intent.putExtra(it.key, it.value as Short)
-                                        "int" -> intent.putExtra(it.key, it.value as Int)
-                                        "long" -> intent.putExtra(it.key, it.value as Long)
-                                        "float" -> intent.putExtra(it.key, it.value as Float)
-                                        "double" -> intent.putExtra(it.key, it.value as Double)
-                                        "char" -> intent.putExtra(it.key, it.value as Char)
-                                        "String" -> intent.putExtra(it.key, it.value as String)
-
-                                        // casting into plural types
-
-                                        "boolean[]" -> intent.putExtra(it.key, it.value as BooleanArray)
-                                        "byte[]" -> intent.putExtra(it.key, it.value as ByteArray)
-                                        "short[]" -> intent.putExtra(it.key, it.value as ShortArray)
-                                        "int[]" -> intent.putExtra(it.key, it.value as IntArray)
-                                        "long[]" -> intent.putExtra(it.key, it.value as LongArray)
-                                        "float[]" -> intent.putExtra(it.key, it.value as FloatArray)
-                                        "double[]" -> intent.putExtra(it.key, it.value as DoubleArray)
-                                        "char[]" -> intent.putExtra(it.key, it.value as CharArray)
-                                        "String[]" -> {
-                                            val tmp = it.value as ArrayList<*>
-                                            intent.putExtra(it.key, tmp.toArray(arrayOfNulls<String>(tmp.size)))
-                                        }
-                                        // if some unsupported type information supplied by user
-                                        else -> intent.putExtra(it.key, it.value as String)
-                                    }
-
-                                } else {
-                                    intent.putExtra(it.key, it.value as String)
-                                }
-                            }
-                        }
-                    }
-                }
-                call.argument<List<Int>>("flag")?.forEach {
-                    intent.addFlags(it)
-                }
-                call.argument<List<String>>("category")?.forEach {
-                    intent.addCategory(it)
-                }
                 try {
                     if (intent.action == MediaStore.ACTION_IMAGE_CAPTURE) {
                         intent.resolveActivity(activity.packageManager).also {
@@ -298,7 +124,7 @@ class IntentPlugin(private val registrar: Registrar, private val activity: Activ
                             }
                         }
                     } else {
-                        if (call.argument<Boolean>("chooser")!!) activity.startActivityForResult(Intent.createChooser(intent, call.argument<String>("chooserTitle") ?: "Sharing"), activityIdentifierCode)
+                        if (call.argument<Boolean>("chooser")!!) activity.startActivityForResult(Intent.createChooser(intent, "Sharing"), activityIdentifierCode)
                         else activity.startActivityForResult(intent, activityIdentifierCode)
                     }
                 } catch (e: Exception) {
@@ -307,6 +133,118 @@ class IntentPlugin(private val registrar: Registrar, private val activity: Activ
             }
             else -> result.notImplemented()
         }
+    }
+
+    private fun createIntentWithMethodCall(call: MethodCall): Intent {
+        val intent = Intent()
+        intent.action = call.argument<String>("action")
+        if (call.argument<String>("package") != null)
+            intent.`package` = call.argument<String>("package")
+
+        val _data = call.argument<String>("data")
+        val _type = call.argument<String>("type")
+        if (_data != null && _type == null)
+            intent.data = Uri.parse(_data);
+        if (_type != null && _data == null)
+            intent.type = _type;
+        if (_type != null && _data != null) {
+            intent.setDataAndType(Uri.parse(_data), _type);
+        }
+
+        // typeInfo parsed into associative array, which can be used for type casting extra data
+        val typeInfo = call.argument<Map<String, String>>("typeInfo")
+                val intent = Intent()
+                intent.action = call.argument<String>("action")
+                if (call.argument<String>("package") != null)
+                    intent.`package` = call.argument<String>("package")
+
+                val _data = call.argument<String>("data")
+                val _type = call.argument<String>("type")
+                if (_data != null && _type == null)
+                    intent.data = Uri.parse(_data);
+                if (_type != null && _data == null)
+                    intent.type = _type;
+                if (_type != null && _data != null) {
+                    intent.setDataAndType(Uri.parse(_data), _type);
+                }
+
+                // typeInfo parsed into associative array, which can be used for type casting extra data
+                val typeInfo = call.argument<Map<String, String>>("typeInfo")
+
+        call.argument<Map<String, Any>>("extra")?.apply {
+            this.entries.forEach {
+                when (it.key) {
+                    Intent.EXTRA_DONT_KILL_APP,
+                    Intent.EXTRA_LOCAL_ONLY,
+                    Intent.EXTRA_ALLOW_MULTIPLE, Intent.EXTRA_PROCESS_TEXT_READONLY -> intent.putExtra(it.key, it.value as Boolean)
+                    Intent.EXTRA_EMAIL,
+                    Intent.EXTRA_BCC,
+                    Intent.EXTRA_CC, Intent.EXTRA_MIME_TYPES -> {
+                        val tmp = it.value as ArrayList<*>
+                        intent.putExtra(it.key, tmp.toArray(arrayOfNulls<String>(tmp.size)))
+                    }
+                    Intent.EXTRA_CONTENT_ANNOTATIONS -> intent.putExtra(it.key, (it.value as? ArrayList<*>)?.filterIsInstance<String>() as ArrayList<String>)
+                    Intent.EXTRA_ORIGINATING_URI -> intent.putExtra(it.key, it.value as Uri)
+                    Intent.EXTRA_PROCESS_TEXT, Intent.EXTRA_TEXT, Intent.EXTRA_TITLE -> {
+                        if (listOf(Intent.ACTION_WEB_SEARCH, Intent.ACTION_SEARCH).contains(intent.action!!))
+                            intent.putExtra("query", it.value as CharSequence)
+                        else
+                            intent.putExtra(it.key, it.value as CharSequence)
+                    }
+                    // here in this block, we'll try to leverage type information
+                    // field passed via platform channel
+                    else -> {
+                        // if type information for this extra key is
+                        // provided by developer, then use that type information
+                        if (typeInfo?.containsKey(it.key)!!) {
+
+                            when (typeInfo[it.key]) {
+                                // casting into singular types
+
+                                "boolean" -> intent.putExtra(it.key, it.value as Boolean)
+                                "byte" -> intent.putExtra(it.key, it.value as Byte)
+                                "short" -> intent.putExtra(it.key, it.value as Short)
+                                "int" -> intent.putExtra(it.key, it.value as Int)
+                                "long" -> intent.putExtra(it.key, it.value as Long)
+                                "float" -> intent.putExtra(it.key, it.value as Float)
+                                "double" -> intent.putExtra(it.key, it.value as Double)
+                                "char" -> intent.putExtra(it.key, it.value as Char)
+                                "String" -> intent.putExtra(it.key, it.value as String)
+
+                                // casting into plural types
+
+                                "boolean[]" -> intent.putExtra(it.key, it.value as BooleanArray)
+                                "byte[]" -> intent.putExtra(it.key, it.value as ByteArray)
+                                "short[]" -> intent.putExtra(it.key, it.value as ShortArray)
+                                "int[]" -> intent.putExtra(it.key, it.value as IntArray)
+                                "long[]" -> intent.putExtra(it.key, it.value as LongArray)
+                                "float[]" -> intent.putExtra(it.key, it.value as FloatArray)
+                                "double[]" -> intent.putExtra(it.key, it.value as DoubleArray)
+                                "char[]" -> intent.putExtra(it.key, it.value as CharArray)
+                                "String[]" -> {
+                                    val tmp = it.value as ArrayList<*>
+                                    intent.putExtra(it.key, tmp.toArray(arrayOfNulls<String>(tmp.size)))
+                                }
+                                // if some unsupported type information supplied by user
+                                else -> intent.putExtra(it.key, it.value as String)
+                            }
+
+                        } else {
+                            intent.putExtra(it.key, it.value as String)
+                        }
+                    }
+                }
+            }
+        }
+        call.argument<List<Int>>("flag")?.forEach {
+            intent.addFlags(it)
+        }
+        call.argument<List<String>>("category")?.forEach {
+            intent.addCategory(it)
+        }
+        if (call.argument<String>("type") != null)
+            intent.type = call.argument<String>("type")
+        return intent
     }
 
     private fun getImageTempFile(): File? {
